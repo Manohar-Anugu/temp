979f20fbdf708b0c9afecc825cce26a32a329203
AOZ <sudhansu.kumar@moschip.com>
2023-08-18 16:59:17 +0530

    Added comment

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-
+//Test code
 class MainActivity : AppCompatActivity() {
     var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
